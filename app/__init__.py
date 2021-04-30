4e2aa2c4dc1875b6b1146b2486a2dd584ab98294
Pavithran V <79012930+its-pavithran-v@users.noreply.github.com>
2021-04-30 21:47:46 +0530

    linked database

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import psycopg2
 import flask,requests,json 
 import jinja2
 from authlib.flask.client import OAuth
@@ -20,5 +21,10 @@ def abc():
 
 @app.route("/test")
 def test():
-    data = "Pavithran"
-    return flask.render_template("/test.html",info = data)
+    server = "postgres://mmojyviutqpftt:9708f3980b74c50a5d4d3ae9713254561626628fde9b23e19e92bfda0ba63b23@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d6pleq228gs43m"
+    connection = psycopg2.connect(server,sslmode = "require")
+    cursor = connection.cursor()
+    query = "SELECT id,url,lastlogin FROM students ORDER BY id ASC"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return flask.render_template("/test.html",info = results)
