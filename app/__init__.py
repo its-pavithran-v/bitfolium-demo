3cf3d43e73c6aebed6d02cd8a92c82fcf27be6a0
Pavithran V <79012930+its-pavithran-v@users.noreply.github.com>
2021-07-10 11:19:47 +0530

    ajax files added
    removed additional pages and added ajax

--- app/__init__.py
+++ app/__init__.py
@@ -2,51 +2,45 @@ import psycopg2
 import flask,requests,json 
 import jinja2
 from authlib.flask.client import OAuth
-from flask import request,redirect,url_for
+from flask import request,redirect,url_for,jsonify,render_template
 
 app = flask.Flask(__name__)
+
+server = "postgres://mmojyviutqpftt:9708f3980b74c50a5d4d3ae9713254561626628fde9b23e19e92bfda0ba63b23@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d6pleq228gs43m"
+connection = psycopg2.connect(server,sslmode = "require")
+cursor = connection.cursor()
 
 @app.route("/", methods=["POST","GET"])
 def test():
 
     if request.method == "GET":
-        server = "postgres://mmojyviutqpftt:9708f3980b74c50a5d4d3ae9713254561626628fde9b23e19e92bfda0ba63b23@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d6pleq228gs43m"
-        connection = psycopg2.connect(server,sslmode = "require")
-        cursor = connection.cursor()
         query = "SELECT students.alias,subscriptions.location, subscriptions.profession, subscriptions.subscriptiontype, subscriptions.enddate, students.url, students.id FROM public.students INNER JOIN public.subscriptions ON students.id=subscriptions.id"
         cursor.execute(query)
         results = cursor.fetchall()
         return flask.render_template("/test.html",info = results)
 
-    if request.method == "POST":
-        user_id = request.form["user_id"]
-        return redirect(url_for("edit",usr_id =user_id))
         
-@app.route("/<usr_id>", methods=["POST","GET"])
-def edit(usr_id):
-
-    user_id = f"{usr_id}"
-
-    if request.method == "GET":
-        server = "postgres://mmojyviutqpftt:9708f3980b74c50a5d4d3ae9713254561626628fde9b23e19e92bfda0ba63b23@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d6pleq228gs43m"
-        connection = psycopg2.connect(server,sslmode = "require")
-        cursor = connection.cursor()
+@app.route("/ajaxfile", methods=["POST","GET"])
+def ajaxfile():
+    
+    if request.method == "POST":
+        user_id = request.form['user_id']
         query = "SELECT students.alias,subscriptions.location, subscriptions.profession, subscriptions.subscriptiontype, subscriptions.enddate, students.url FROM public.students INNER JOIN public.subscriptions ON students.id=%s AND subscriptions.id=%s"
         tuple1 = (user_id,user_id)
         cursor.execute(query,tuple1)
         results = cursor.fetchall()
-        return flask.render_template("/edit_data.html",data = results)
+    return jsonify({'htmlresponse': render_template('response.html',results=results,user_id=user_id)})
 
+@app.route("/ajaxfile1", methods=["POST","GET"])
+def ajaxfile1():
     if request.method == "POST":
+        user_id = request.form['user_id']
         name = request.form["name"]
         location = request.form["location"]
         profession = request.form["profession"]
         account_type = request.form["account_type"]
         subscription_enddate = request.form["subscription_enddate"]
         url = request.form["url"]        
-        server = "postgres://mmojyviutqpftt:9708f3980b74c50a5d4d3ae9713254561626628fde9b23e19e92bfda0ba63b23@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d6pleq228gs43m"
-        connection = psycopg2.connect(server,sslmode = "require")
-        cursor = connection.cursor()
         query1 = "UPDATE students SET alias=%s , url=%s WHERE id=%s"
         tuple1 = (name,url,user_id)
         cursor.execute(query1,tuple1)
@@ -54,4 +48,4 @@ def edit(usr_id):
         tuple2 = (location,profession,account_type,subscription_enddate,user_id)
         cursor.execute(query2,tuple2)
         connection.commit()
-        return redirect(url_for("test"))
+    return "success"
